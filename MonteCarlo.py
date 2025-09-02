7c23da3096f23b6440cea3a80c433614391f6f21
Jimmmmies <2099869463@qq.com>
2025-09-02 15:54:05 +0800

    update learning progress

--- MonteCarlo.py
+++ MonteCarlo.py
@@ -2,9 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import gymnasium as gym
 
-# 5 7 11 12
-# 左 下 右 上
-# prob next_s reward done
 class MonteCarlo_epsilon_greedy:
     
     def __init__(self):
@@ -14,16 +11,16 @@ class MonteCarlo_epsilon_greedy:
         self.n_actions = self.env.action_space.n
         self.Q = np.zeros((self.n_states, self.n_actions))
         self.N = np.zeros((self.n_states, self.n_actions))
-        self.policy = np.zeros(self.n_states)
         self.V = np.zeros(self.n_states)
+        self.policy = np.zeros(self.n_states)
         self.holes = [5, 7, 11, 12]
         self.end = [15]
 
     def epsilon_greedy(self, state, epsilon=0.1):
-        if np.random.rand() < epsilon:
-            return np.argmax([np.random.randint(self.n_actions)])
-        else:
-            return np.argmax(self.Q[state])
+        best_action = np.argmax(self.Q[state])
+        probs = np.ones(self.n_actions) * (epsilon / self.n_actions)
+        probs[best_action] += (1.0 - epsilon)
+        return np.random.choice(np.arange(self.n_actions), p=probs)
 
     def sample_episode(self, episode_maxlength, epsilon=0.1):
         episode = []
@@ -32,38 +29,46 @@ class MonteCarlo_epsilon_greedy:
         while episode_len < episode_maxlength:
             action = self.epsilon_greedy(state, epsilon)
             next_state, reward, terminated, truncated, _ = self.env.step(action)
-            episode.append((state, action, reward, next_state))
+            if terminated and next_state in self.end:
+                custom_reward = 5
+            elif terminated:
+                custom_reward = -10
+            else:
+                custom_reward = -1.5
+            episode.append((state, action, custom_reward, next_state))
             if terminated or truncated:
                 break
             episode_len += 1
             state = next_state
         return episode
 
-    def update_policy(self, episodes_num, episode_maxlength, gamma=0.9, epsilon=0.1):
+    def update_policy(self, episodes_num, episode_maxlength, gamma=0.95, epsilon=0.1):
+        min_epsilon = 0.01
+        epsilon_decay = (epsilon - min_epsilon) / (episodes_num * 0.8)
         for _ in range(episodes_num):
-            if epsilon > 0.01:
-                epsilon -= 0.001
+            epsilon = max(min_epsilon, epsilon - epsilon_decay)
             episode = self.sample_episode(episode_maxlength, epsilon)
             G = 0
+            # visited = set()
             for i in range(len(episode) - 1, -1, -1):
                 state, action, reward, next_state = episode[i]
                 G = reward + gamma * G
+                # if (state, action) not in visited:
+                    # visited.add((state, action))
                 self.N[state, action] += 1
                 self.Q[state, action] += (G - self.Q[state, action]) / self.N[state, action]
         for s in range(self.n_states):
-            best_action = np.argmax(self.Q[s])
-            self.policy[s] = best_action
-            for a in range(self.n_actions):
-                if a == best_action:
-                    self.V[s] += (1 - (self.n_actions - 1) * epsilon / self.n_actions) * self.Q[s][a]
-                else:
-                    self.V[s] += epsilon / self.n_actions * self.Q[s][a]
+            self.V[s] = np.max(self.Q[s])
 
     def display_policy(self):
+        for s in range(self.n_states):
+            self.policy[s] = np.argmax(self.Q[s])
         for i in range(4):
             for j in range(4):
                 if (i * 4 + j) in self.holes:
                     print("#", end=' ')
+                elif (i * 4 + j) == self.end[0]:
+                    print("🏁", end=' ')
                 elif self.policy[i * 4 + j] == 0:
                     print('←', end=' ')
                 elif self.policy[i * 4 + j] == 1:
@@ -74,9 +79,10 @@ class MonteCarlo_epsilon_greedy:
                     print('↑', end=' ')
             print()
         for s in range(self.n_states):
-            print(f'State {s}: V = {self.V[s]:.2f}, Q = {self.Q[s]}, Policy = {self.policy[s]}')
+            for a in range(self.n_actions):
+                print(self.Q[s, a], end=' ')
 
 if __name__ == '__main__':
     agent = MonteCarlo_epsilon_greedy()
-    agent.update_policy(10000, 100)
+    agent.update_policy(100000, 100)
     agent.display_policy()
